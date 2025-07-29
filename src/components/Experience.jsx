6f92f9523ddf5ae67af8d9ad4476005430382202
MinifyX-hash <mahato963592@gmail.com>
2025-07-29 08:35:00 +0530

    rename experience to internships

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -45,7 +45,7 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()} initial="hidden" whileInView="show">
         <p className={styles.sectionSubText}>What I have done so far</p>
-        <h2 className={styles.sectionHeadText}>Work Experience .</h2>
+        <h2 className={styles.sectionHeadText}>Inernships</h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
