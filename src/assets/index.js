7a5eacc2cb589a9bceb6e4a12cd5c06b98a79484
MinifyX-hash <mahato963592@gmail.com>
2025-07-30 08:30:34 +0530

    work on projects links and datas

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,8 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import weather from "./weather.png";
+import upcoming from "./upcoming.png";
 
 export {
   logo,
@@ -59,4 +61,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  weather,
+  upcoming,
 };
